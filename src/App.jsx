051daec181de80a2c6f0289c52c391759b0f64be
kyrielin <kyrielin@tencent.com>
2023-08-30 16:12:04 +0800

    feat: add repo link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import data from '../public/data.json';
+import githubLogo from './assets/github-mark.svg';
 import SecurityDate from './components/security-date.jsx';
 
 function App() {
@@ -7,6 +8,12 @@ function App() {
     <div className="container mx-auto">
       <div className="">
         <SecurityDate data={data} />
+      </div>
+
+      <div className="fixed bottom-4 right-4">
+        <a href="https://github.com/honkinglin/security-stats" target="_blank">
+          <img width={32} src={githubLogo} alt="github" />
+        </a>
       </div>
     </div>
   )
